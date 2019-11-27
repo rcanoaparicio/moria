2134498eec23d197c3fafabe6ce94ab3a5343ac6
Cano <ramon.canoaparicio@gmail.com>
2019-11-27 11:55:13 +0100

    Test updated

--- test.sh
+++ test.sh
@@ -1,21 +1,58 @@
 #!/bin/bash
-TEST_SIZE=100
-player_name="EPSEVG"
+TEST_SIZE=$5
+
+count1=0
+count2=0
+count3=0
+count4=0
+
+echo "$1 got top score" > win1.line
+echo "$2 got top score" > win2.line
+echo "$3 got top score" > win3.line
+echo "$4 got top score" > win4.line
+
+n=($1 $2 $3 $4)
 
 
-count=0
-echo "$player_name got top score" > win.line
+
 for ((i=0; $i < $TEST_SIZE; i++))
 do
-	./Game Dummy Dummy Dummy EPSEVG -s $i -i default.cnf -o default.res 2> out.err
-	if grep -Fxq out.err win.line
+	r=$RANDOM
+	let "r %= 4"
+	./Game ${n[$r]} ${n[((($r+1)%4))]} ${n[((($r+2)%4))]} ${n[((($r+3)%4))]} -s $i -i default.cnf -o default.res 2> out.err
+	if grep -c "$1 got top score" out.err
 	then
-		let "count+=0"
+		let "count1+=1"
 	else 
-		let "count+=1"
+		let "count1+=0"
+	fi
+
+	if grep -c "$2 got top score" out.err
+	then
+		let "count2+=1"
+	else 
+		let "count2+=0"
+	fi
+
+	if grep -c "$3 got top score" out.err
+	then
+		let "count3+=1"
+	else 
+		let "count3+=0"
+	fi
+
+	if grep -c "$4 got top score" out.err
+	then
+		let "count4+=1"
+	else 
+		let "count4+=0"
 	fi
 	rm out.err
 	echo "game $i completed"
 done
-rm win.line
-echo "Partidas ganadas: $count de $TEST_SIZE"
+rm win1.line win2.line win3.line win4.line
+echo "Resultados:"
+echo "  $1: $count1"
+echo "  $2: $count2"
+echo "  $3: $count3"
+echo "  $4: $count4"
